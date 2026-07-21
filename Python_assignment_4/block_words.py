463904c50bc9b2a4beb4debbfcf630c5ca663d5c
Tarun <talaganatarun123@gmail.com>
2026-07-21 23:21:43 +0530

    df

---
+++ Python_assignment_4/block_words.py
@@ -0,0 +1,5 @@
+def block_words():
+    blocked_list=["bastard","dog","donkey","stupid"]
+    text=input.split()
+    for i in range(0,len(text)):
+        final_text=text.replace
--- Python_assignment_4/id_card_name.py
+++ Python_assignment_4/id_card_name.py
@@ -2,6 +2,6 @@ def id_name():
     name=input().split()
     id_name=""
     for i in range(0,len(name)):
-        id_name+=name[i][1]
+        id_name+=name[i][0].upper()
     print(id_name)
 id_name()
